3463ba8cfa07d62a0554f39bbe46c17a023696bc
Andrew Scholer <ascholer@chemeketa.edu>
2025-05-07 13:34:59 -0700

    build script update

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cmd="python /mnt/f/Programming/pretext/pretext/pretext -f html -c all -d out -vvv -p publication/publication.xml source/main.ptx -r functions-testing -x debug.rs.dev yes html.quick-dirty yes"
-cmd="python /mnt/f/Programming/pretext/pretext/pretext -f html -c all -d published/tourcs -vvv -p publication/publication-runestone.xml source/main.ptx -r vector-references -x debug.rs.dev yes html.quick-dirty yes"
+# cmd="python /mnt/f/Programming/pretext/pretext/pretext -f html -c all -d out -vvv -p publication/publication.xml source/main.ptx -r functions-testing -x debug.rs.dev yes html.quick-dirty yes"
+cmd="python /mnt/c/Users/Andrew/Programming/pretext/pretext/pretext -f html-incremental -c all -d published/tourcs -vvv -p publication/publication-runestone.xml source/main.ptx -r pointers-aggregation -x debug.rs.dev yes html.build-preview yes"
 #cmd="python /mnt/f/Programming/pretext/pretext/pretext -f html -c all -d published/tourcs -vvv -p publication/publication-runestone.xml source/main.ptx -x debug.rs.dev yes html.quick-dirty yes"
 #cmd="python /mnt/f/Programming/pretext/pretext/pretext -f html -c all -d published/tourcs -vvv -p publication/publication-runestone.xml source/main.ptx"
 $cmd
+``
