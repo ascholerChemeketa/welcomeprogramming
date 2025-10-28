477162c08640a7fc3b5b0ac41875d38d1df45846
Andrew Scholer <ascholer@chemeketa.edu>
2025-10-28 15:45:00 -0700

    Ch 9 fixups

--- programs/strings/other-string-functions-3.cpp
+++ programs/strings/other-string-functions-3.cpp
@@ -1,2 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
     string message = "Hello world!";
     message.replace(6, 5, "you");
+    cout << message << endl;
+}
