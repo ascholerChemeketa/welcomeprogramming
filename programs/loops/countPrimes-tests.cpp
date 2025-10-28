d24743fbc9e5da215f69f29709e3a11ffad205de
Andrew Scholer <ascholer@chemeketa.edu>
2025-10-28 11:38:00 -0700

    Count primes tests

--- programs/loops/countPrimes-tests.cpp
+++ programs/loops/countPrimes-tests.cpp
@@ -8,6 +8,7 @@ TEST_CASE("countPrimes") {
     CHECK(countPrimes(2) == 1);
     CHECK(countPrimes(4) == 2);
     CHECK(countPrimes(10) == 4);
-    CHECK(countPrimes(11) == 4);
+    CHECK(countPrimes(11) == 5);
+    CHECK(countPrimes(12) == 5);
     CHECK(countPrimes(100) == 25);
 }
