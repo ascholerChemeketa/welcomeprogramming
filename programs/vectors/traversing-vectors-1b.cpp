0c82af5cd808a19bbb05e583260aae0cc0bfa9d5
Andrew Scholer <ascholer@chemeketa.edu>
2025-11-24 12:01:19 -0800

    Typo fix vectors

--- programs/vectors/traversing-vectors-1b.cpp
+++ programs/vectors/traversing-vectors-1b.cpp
@@ -5,9 +5,13 @@ using namespace std;
 
 int main() {
     vector<int> a = {1, 2, 3, 4, 5};
+
     for (size_t i = 0; i < a.size(); ++i) {
-        a.at(i) = pow(a.at(i), 2); // square each element
+        int oldValue = a.at(i);
+        int newValue = oldValue * oldValue;
+        a.at(i) = newValue; // set the new value at index i
     }
+
     // print the modified vector on one line
     for (size_t i = 0; i < a.size(); ++i) {
         cout << a.at(i) << " ";
