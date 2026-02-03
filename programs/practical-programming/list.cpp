bb669f2cc51a01a63d3f5f4d287f17d6afc1263b
Andrew Scholer <ascholer@chemeketa.edu>
2026-02-03 13:56:11 -0800

    More list

--- programs/practical-programming/list.cpp
+++ programs/practical-programming/list.cpp
@@ -12,22 +12,26 @@ int main() {
     jobs.push_back("jobB");
     jobs.push_back("jobC");
 
+    cout << "Front: " << jobs.front() << endl;
+    cout << "Back: " << jobs.back() << endl;
+    cout << "Size: " << jobs.size() << endl;
+
     // Add an urgent job at the front
     jobs.push_front("urgent");
 
-    cout << "Front: " << jobs.front() << "\n";
-    cout << "Back: " << jobs.back() << "\n";
+    cout << "Front: " << jobs.front() << endl;
+    cout << "Size: " << jobs.size() << endl;
 
     // Process jobs FIFO: pop from the front
     while (!jobs.empty()) {
-        cout << "Processing: " << jobs.front() << "\n";
+        cout << "Processing: " << jobs.front() << endl;
         jobs.pop_front();
     }
 
     // Demonstrate back operations
     jobs.push_back("cleanup1");
     jobs.push_back("cleanup2");
-    cout << "Removing from back: " << jobs.back() << "\n";
+    cout << "Removing from back: " << jobs.back() << endl;
     jobs.pop_back();
-    cout << "Remaining size: " << jobs.size() << "\n";
+    cout << "New back: " << jobs.back() << endl;
 }
