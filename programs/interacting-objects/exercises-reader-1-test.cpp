6cb61e1ee0f8e61bac46d8edf778601e5da7a907
Andrew Scholer <ascholer@chemeketa.edu>
2026-04-09 15:31:52 -0700

    One more fix

--- programs/interacting-objects/exercises-reader-1-test.cpp
+++ programs/interacting-objects/exercises-reader-1-test.cpp
@@ -3,15 +3,16 @@
 #include "doctest.h"
 
 TEST_CASE("Test Reader::readBook") {
-    Book b1("Book 1", 100);
-    Book b2("Book 2", 200);
+    Book b1("Book1", 100);
+    Book b2("Book2", 200);
+
     Reader r("Alice");
     r.readBook(b1);
 
-    string expected = "Book 1";
+    string expected = "Book1";
     CHECK(r.getHistory() == expected);
 
     r.readBook(b2);
-    expected = "Book 2";
+    expected = "Book1 Book2";
     CHECK(r.getHistory() == expected);
 }
