4e0a25a9ac63b3a0c6e940d0c8fc134541e9ce89
Andrew Scholer <ascholer@chemeketa.edu>
2026-04-20 08:57:39 -0700

    ch17 file include fix

--- programs/interacting-objects/in-vectors-2.cpp
+++ programs/interacting-objects/in-vectors-2.cpp
@@ -6,6 +6,6 @@ import SimplePoint; // Import the module with SimplePoint1 and SimplePoint2
 
 int main()
 {
-    // Try to create a vector of SimplePoint1 objects
+    // Try to create a vector of 5 SimplePoint1 objects
     vector<SimplePoint1> points(5);
 }
